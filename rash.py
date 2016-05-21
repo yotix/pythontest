a1e58958875746f893ed3469e3a28c2c48f3b9b8
yotix <yogesh.bhardwaj@hotmail.com>
2016-05-22 00:38:49 +0530

    Update rash.py

--- rash.py
+++ rash.py
@@ -1,10 +1,11 @@
 def reversehash(h):
-    s =''
+    s = []
     a = 'acdegilmnoprstuw'
     for x in xrange(9):
-        s += a[h%37]
+        s.append(a[h%37])
         #print a[h%37]
         h = (h-h%37)/37
-    return s
-
+    s.reverse()
+    return ('').join(s)
+    
 print reversehash(930846109532517)
