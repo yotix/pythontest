78a37f3d7f6461ac223fd62922d38009817dce2c
yotix <yogesh.bhardwaj@hotmail.com>
2016-05-23 09:21:21 +0530

    Update rash.py

--- rash.py
+++ rash.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+import random
+
+def hash(s):
+    h = 9
+    t = 'acdegilmnoprstuw'
+    for x in xrange(len(s)):
+        h = (h*37+t.index(s[x]))
+    return h 
+    pass
+
 def reversehash(h):
     s = []
     a = 'acdegilmnoprstuw'
@@ -7,5 +18,13 @@ def reversehash(h):
         h = (h-h%37)/37
     s.reverse()
     return ('').join(s)
+
+def unittesting():
+    a = 'acdegilmnoprstuw'
+    s = ''
+    for x in xrange(9):
+        s += a[random.randrange(0,16,2)]
+    print reversehash(hash(s)) == s,hash(s),s,reversehash(hash(s))
     
-print reversehash(930846109532517)
+for x in xrange(10):
+    print unittesting()
